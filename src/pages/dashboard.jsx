38996ebffed50f2c7013ecfaa55fce9041db1565
Lyna Chalal <kyysmkym@gmail.com>
2024-10-25 11:57:44 +0100

    test

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -6,7 +6,7 @@ import Chart from '../components/chart.jsx';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
 const user = {
-  name: 'Tom Ancien',
+  name: 'Tom New',
   email: 'tom@example.com',
   imageUrl: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
